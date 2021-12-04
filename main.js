bd9cd88c24016130116c5975e9c0481fa7b86431
inoisy <inoisy@bk.ru>
2021-12-04 12:40:11 +0300

    fix

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ import puppeteer from 'puppeteer';
 import cheerio from 'cheerio';
 import fs from 'fs';
 import csvWriter from 'csv-writer'
-
+import cliProgress from 'cli-progress'
 class Logger {
     constructor(name, folderName) {
-        // this.init(name, folderName)
         return new Promise(async (resolve, reject) => {
             if (!(await fs.existsSync(folderName))) {
                 await fs.mkdirSync(folderName)
@@ -19,22 +18,16 @@ class Logger {
                 flags: 'a'
             })
             this.logger = (new console.Console(stream))
-            // this.bind(new console.Console(stream))
             resolve(this.logger)
         });
     }
 }
 if (!String.prototype.replaceAll) {
     String.prototype.replaceAll = function (str, newStr) {
-
-        // If a regex pattern
         if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
             return this.replace(str, newStr);
         }
-
-        // If a string
         return this.replace(new RegExp(str, 'g'), newStr);
-
     };
 }
 
@@ -44,7 +37,7 @@ async function launchPuppeteer(headless = false) {
     });
     const page = await browser.newPage();
 
-    return page
+    return { page, browser }
 }
 
 function sleep(ms) {
@@ -54,39 +47,24 @@ function sleep(ms) {
         }, ms)
     })
 }
-async function getLinks() {
-    console.log("🚀 ~ file: main.js ~ line 4 ~ main ~ main")
-    const page = await launchPuppeteer()
 
-    await page.goto('https://cbr.ru/cash_circulation/memorable_coins/coins_base/')
-    await page.evaluate(async () => {
-        document.querySelector('.filters_wrapper .filters .filter:first-child .filter_title').click()
-        document.querySelector('.filter.open .filter_content .filter-select .filter-select_option:first-child input').click()
-    });
-    await sleep(4000)
-    const items = await page.evaluate(async () => {
-        return Array.from(document.querySelectorAll('.coins-tile .coins-tile_item a')).map(item => item.href)
-    })
 
-    console.log("🚀 ~ file: main.js ~ line 22 ~ awaitpage.evaluate ~ items", items.length, "-----", items[0])
-    await fs.writeFileSync('outputLinks.json', JSON.stringify(items));
-
-}
 function makeAbsoluteLink(string) {
     return 'https://cbr.ru' + string
 }
+
 function cleanString(string) {
     return string.replaceAll('\n', '')
 }
+
 async function fetchItemFromHTML(html) {
-    // console.log("🚀 ~ file: main.js ~ line 41 ~ fetchItemFromHTML ~ html", html)
     const $ = await cheerio.load(html, {
         decodeEntities: false
     })
+    // $('.commemor-coin .commemor-coin_intro.d-none').remove()
     const images = $('.commemor-coin .commemor-coin_images img').get().map(item => makeAbsoluteLink(item.attribs.src))
-    // console.log("🚀 ~ file: main.js ~ line 46 ~ fetchItemFromHTML ~ images", images)
     const title = $('h1').text()
-    const moneyOptions = $('.commemor-coin .commemor-coin_intro:not(.d-none) .money_options .money_option').map((i, el) => {
+    const moneyOptions = $('.commemor-coin .commemor-coin_intro .money_options .money_option').map((i, el) => {
         const key = $(el).find(".money_option_title").text().trim()
         const val = $(el).find(".money_option_value").text().trim()
         return {
@@ -104,44 +82,25 @@ async function fetchItemFromHTML(html) {
     }).get()
     $(`.commemor-coin .commemor-coin_content div[class^="commemor-coin"]`).remove()
     const content = $('.commemor-coin_content').html().trim()
-    // console.log("🚀 ~ file: main.js ~ line 82 ~ fetchItemFromHTML ~ content", content)
-    let splittedContent;
-    try {
-        splittedContent = content.split('<h2>').map(item => item.trim()).filter(item => item).map(item => '<h2>' + item)
-    } catch (error) {
-        console.log("🚀 ~ file: main.js ~ line 86 ~ fetchItemFromHTML ~ error", error)
-    }
+    // let splittedContent;
+    // try {
+    //     splittedContent = content.split('<h2>').map(item => item.trim()).filter(item => item).map(item => '<h2>' + item)
+    // } catch (error) {
+    //     console.log("🚀 ~ file: main.js ~ line 86 ~ fetchItemFromHTML ~ error", error)
+    // }
+    // const content = $('.commemor-coin_content').html().trim()
+    const serie = $('.commemor-coin .commemor-coin_intro.d-none .commemor-coin_intro_text').text().trim()
     return {
         title: cleanString(title),
         images,
         moneyOptions,
         characteristics,
         content,
-        splittedContent
+        serie
+        // splittedContent
     }
-}
-async function downloadItems() {
-    const links = JSON.parse(await fs.readFileSync('outputLinks.json'))
-    console.log("🚀 ~ file: main.js ~ line 43 ~ main ~ links", links[0])
-    const resultArray = []
-    for (let link of links) {
-        try {
-            console.log("🚀 ~ file: main.js ~ line 45 ~ main ~ link", link)
-            const { data: pageHTML } = await axios.get(link)
-            // console.log("🚀 ~ file: main.js ~ line 47 ~ main ~ data", data)
-            const itemData = await fetchItemFromHTML(pageHTML)
-            // console.log("🚀 ~ file: main.js ~ line 53 ~ main ~ itemData", itemData)
-            resultArray.push(itemData)
-        } catch (error) {
-            console.log("🚀 ~ file: main.js ~ line 114 ~ main ~ error", error)
-        }
-        // document.querySelectorAll(".commemor-coin .commemor-coin_images img")
-    }
-    console.log("🚀 ~ file: main.js ~ line 112 ~ main ~ resultArray", resultArray)
-    await fs.writeFileSync('result.json', JSON.stringify(resultArray));
 }
 
-// main()
 function flatFromKeyValue(payload) {
     return payload.reduce((acc, { key, val }) => {
         acc[key] = val
@@ -149,36 +108,90 @@ function flatFromKeyValue(payload) {
     }, {})
 }
 
-function remakeItem({ moneyOptions, characteristics, splittedContent, ...otherProperties }) {
-    // console.log("🚀 ~ file: main.js ~ line 164 ~ remakeItem ~ otherProperties", otherProperties)
-    // console.log("🚀 ~ file: main.js ~ line 164 ~ remakeItem ~ characteristics", )
-    // console.log("🚀 ~ file: main.js ~ line 164 ~ remakeItem ~ moneyOptions",)
-    // console.log("🚀 ~ file: main.js ~ line 159 ~ remakeItem ~ payload", payload)
+function remakeItem({ item, logger }) {
+    const {
+        moneyOptions,
+        images,
+        serie,
+        characteristics,
+        splittedContent,
+        ...otherProperties
+    } = item
+    const imagesJoined = images.reduce((acc, val, index) => {
+        acc[`image${index}`] = val
+        return acc
+    }, {})
+    const serieTransformed = serie.replaceAll("  ", '').trim()
+    logger.log(serieTransformed)
     return {
         ...otherProperties,
+        serie:serieTransformed,
+        ...imagesJoined,
         ...flatFromKeyValue(characteristics),
         ...flatFromKeyValue(moneyOptions)
     }
 }
-function getAllKeys(payload){
+function getAllKeys(payload) {
     const keysSet = new Set()
-    for(let item of payload){
-        Object.keys(item).forEach(it=>keysSet.add(it))
-        // keysSet.add(...Object.keys(item))
+    for (let item of payload) {
+        Object.keys(item).forEach(it => keysSet.add(it))
     }
-    
+
     return Array.from(keysSet)
 }
-async function remake() {
+
+async function getLinks() {
+    const { page, browser } = await launchPuppeteer()
+    await page.goto('https://cbr.ru/cash_circulation/memorable_coins/coins_base/')
+    await page.evaluate(async () => {
+        document.querySelector('.filters_wrapper .filters .filter:first-child .filter_title').click()
+        document.querySelector('.filter.open .filter_content .filter-select .filter-select_option:first-child input').click()
+    });
+    await sleep(4000)
+    const items = await page.evaluate(async () => {
+        return Array.from(document.querySelectorAll('.coins-tile .coins-tile_item a')).map(item => item.href)
+    })
+    await fs.writeFileSync('outputLinks1.json', JSON.stringify(items));
+    await page.close();
+    await browser.close()
+}
+
+async function downloadItems() {
+    const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    const links = JSON.parse(await fs.readFileSync('outputLinks1.json'))
+    const resultArray = []
+    const itemsLength = links.length
+    progress.start(itemsLength, 0);
+    for (let i = 0; i < itemsLength; i++) {
+        const link = links[i]
+        try {
+            const { data: pageHTML } = await axios.get(link)
+            const itemData = await fetchItemFromHTML(pageHTML)
+            resultArray.push(itemData)
+            progress.update(i);
+        } catch (error) {
+            progress.update(i);
+            console.log("🚀 ~ file: main.js ~ line 114 ~ main ~ error", error)
+        }
+    }
+    progress.update(itemsLength);
+    progress.stop();
+    await fs.writeFileSync('result1.json', JSON.stringify(resultArray));
+}
+
+async function remake({ logger }) {
+    // const progress = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
     const createCsvWriter = csvWriter.createObjectCsvWriter;
 
-    const logger = await new Logger("remake", "logs")
-    const items = JSON.parse(await fs.readFileSync("./result.json"))
-    // console.log("🚀 ~ file: main.js ~ line 159 ~ remake ~ items", items[0])
-    const transformedItems = items.map(item => remakeItem(item))
+    const items = JSON.parse(await fs.readFileSync("./result1.json"))
+    // const itemsLength = links.length
+    // for (let i = 0; i < itemsLength; i++ ) {
+
+    // }
+    const transformedItems = items.map(item => remakeItem({ item, logger }))
     const headers = getAllKeys(transformedItems)
     const csvWriterInstance = createCsvWriter({
-        path: 'output.csv',
+        path: 'output6.csv',
         header: headers.map(item => {
             return {
                 id: item,
@@ -187,20 +200,14 @@ async function remake() {
         })
     });
     await csvWriterInstance.writeRecords(transformedItems)
-    console.log("🚀 ~ file: main.js ~ line 182 ~ remake ~ transformedItems")
 }
-remake()
-
-async function writeToCSV({headers,path}) {
-    let itemsNew = []
-    const csvWriter = createCsvWriter({
-        path: 'outputChisto.v2.csv',
-        // header: headers.map(item => {
-        //     return {
-        //         id: item,
-        //         title: item
-        //     }
-        // })
-    });
-    await csvWriter.writeRecords(payload)
+// remake()
+async function main() {
+    const logger = await new Logger("remake", "logs")
+    console.log("Process start")
+    // await getLinks()
+    // await downloadItems()
+    await remake({ logger })
+    console.log("Process end")
 }
+main()
